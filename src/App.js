6af1614d8811aef3658deb03cffa6f5a47865f16
gangwarpriyanshi@gmail.com <gangwarpriyanshi@gmail.com>
2020-12-28 17:23:36 +0530

    before using redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React,{useEffect} from 'react';
+import React,{Fragment,useEffect} from 'react';
+import Searchbar from './components/layout/Searchbar'
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn'
+import AddLogModal from './components/logs/AddLogModal'
+import EditLogModal from './components/logs/EditLogModal'
+import AddTechModal from './components/techs/AddTechModal'
+import TechListModal from './components/techs/TechListModal'
+
+
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js'
+
+
 //const App = () =>{}
+
 function App() {
-  useEffect(()=> {
-    //initilize mate-.js
-    M.AutoInit();
-  });
+        useEffect( ()=> {M.AutoInit();} );
+
+
   return (
-    <div className="App">
-    My App
+  <Fragment>
+    <Searchbar/>
+    <div className="container">
+      <AddBtn/>
+      <AddLogModal/>
+      <EditLogModal/>
+      <AddTechModal/>
+      <TechListModal/>
+      <Logs/>
     </div>
+
+  </Fragment>
+
   );
 }
 
